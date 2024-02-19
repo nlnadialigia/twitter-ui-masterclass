b05ad67d632ba58a7d57ed5dee32e69a24b4018b
Nadia <nlnadialigia@gmail.com>
2024-02-18 21:04:54 -0300

    feat: save new tweets

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,19 +1,33 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 import "./Timeline.css";
 
-const tweets = ["A", "B", "C"];
+let newTweet = "";
 
 export function Timeline() {
+  const [tweets, setTweets] = useState(["A", "B", "C"]);
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+    setTweets([...tweets, newTweet]);
+  }
+
   return (
     <main className="timeLine">
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form className="new-tweet-form" onSubmit={createNewTweet}>
         <label htmlFor="tweet">
           <img src="https://github.com/nlnadialigia.png" alt="Nadia Ligia" />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            onChange={(event) => {
+              newTweet = event.target.value;
+            }}
+          />
         </label>
         <button type="submit">Tweet</button>
       </form>
